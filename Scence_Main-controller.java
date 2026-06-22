37fb7ac8a4af613faaaed13f16acfcf1f87b2b56
Starling <nakboyloy50@gmail.com>
2026-06-22 15:50:10 +0700

    up

---
+++ Scence_Main-controller.java
@@ -0,0 +1,34 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class Scence_Main-controller {
+
+    @FXML
+    private ListView<?> list_view;
+
+    @FXML
+    private Label status;
+
+    @FXML
+    void On_create(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_read(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_update(ActionEvent event) {
+
+    }
+
+}
