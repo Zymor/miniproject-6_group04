24faa86a8b1b918d2a121c501d24729518b1d511
Starling <nakboyloy50@gmail.com>
2026-06-22 16:59:25 +0700

    up

---
+++ Main.java
@@ -0,0 +1,27 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+  @Override
+    public void start(Stage stage) throws Exception {
+        var view_main = getClass().getResource("scence_main/View.fxml");
+        var controller_main = new scence_main.Controller();
+
+        var loader = new FXMLLoader();
+        loader.setLocation(view_main);
+        loader.setController(controller_main);
+
+
+        var scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ scence_main/Controller.java
@@ -0,0 +1,62 @@
+package scence_main;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.FileChooser;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label Status;
+
+    @FXML
+    private ListView<?> list_view;
+
+    @FXML
+    void On_Create(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_Read(ActionEvent event) {
+        var stage = (Stage) Status.getScene().getWindow();
+        var file_chooser = new FileChooser();
+        var selected = file_chooser.showOpenDialog(stage);
+
+        var data = new ArrayList<String>();
+        for (var line : Files.readAllLines(selected.toPath())) {
+            data.add(line);
+        }
+        list_view.getItems().addAll(data);
+    }
+
+    @FXML
+    void On_delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_update(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+    }
+
+}
