b023da43c5c876cda3e144e6cddd0b0c13feed16
Starling <nakboyloy50@gmail.com>
2026-06-24 13:30:01 +0700

    up

---
+++ scence_delete/Controller.java
@@ -0,0 +1,31 @@
+package scence_delete;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void On_no(ActionEvent event) {
+
+    }
+
+    @FXML
+    void On_yes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
---
+++ scence_update/Controller.java
@@ -0,0 +1,31 @@
+package scence_update;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField Text_field;
+
+    @FXML
+    void On_save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert Text_field != null : "fx:id=\"Text_field\" was not injected: check your FXML file 'View.fxml'.";
+
+    }
+
+}
